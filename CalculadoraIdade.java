0d26396fd48fc0ffd603b7e08a0cfb5cccc8440c
pmanjos <magalhaesdosanjos@gmail.com>
2025-06-27 18:39:28 -0300

    Primeiro commit: Adicionado CalculadoraIdade.java

---
+++ CalculadoraIdade.java
@@ -0,0 +1,9 @@
+public class CalculadoraIdade {
+    public static void main(String[] args) { // Correção aqui, deve ser String[]
+        int anoAtual = 2025;
+        int anoNascimento = 1996;
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println("Sua idade é: " + idade + " anos.");
+    }
+}
